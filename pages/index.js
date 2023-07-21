42bfb594b91c623ebb77a330443933583bd89b4d
nguyenquangduczoitech <duc.nguyenquang@zoi.tech>
2023-07-21 13:39:19 +0700

    add rss news

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link';
 import Date from '../components/date';
+import Parser from 'rss-parser';
+
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -15,13 +18,26 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+  let parser = new Parser();
+  
+  const getRss = (async () => {
+    let feed = await parser.parseURL('https://news.yahoo.com/rss/world');
+    console.log(feed.title);
+  
+    feed.items.forEach(item => {
+      console.log(item.title + ':' + item.link)
+    });
+  
+  });
+  getRss();
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[Your Self Introduction 111]</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
